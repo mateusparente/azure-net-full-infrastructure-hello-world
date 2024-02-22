90d051f5120d2f78d080af743217e102759fbd20
Mateus Parente <contato@mateusparente.com.br>
2024-02-22 23:23:21 +0000

    task-000 adding swagger to test integration with azure api management

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -1,6 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "My API", Version = "v1" });
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"));
+}
+
+app.MapControllers();
+app.MapGet("/hello", () => "Hello World!").WithName("GetHelloWorld");;
 
 app.Run();
